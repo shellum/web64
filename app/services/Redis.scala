40cc6e925ce54fa7e79bf5a61105a4f47d9b9457
Cameron Shellum <shellum@gmail.com>
2014-08-28 16:40:35 -0600

    Fix column/fix test

--- app/services/Redis.scala
+++ app/services/Redis.scala
@@ -12,32 +12,32 @@ abstract class Db()
 @Singleton
 class Redis @Inject()(@Named("Redis Host") host: String) extends Db() {
 
-  var jedisInstance: Jedis = _
+  var jedisInstance: Jedis = null
 
   def setJedis(jedis: Jedis) = {
     jedisInstance = jedis
   }
 
-  def getJedis = {
+  def getJedis(): Jedis = {
     if (jedisInstance == null)
       jedisInstance = new Jedis(host)
-    new Jedis(host)
+    jedisInstance
   }
 
   def delVals(myKey: String) = {
-    jedisInstance.del(myKey)
+    getJedis().del(myKey)
   }
 
   def addGradient(myVal1: String, myVal2: String) = {
-    val myKey = jedisInstance.incr("primary")
-    jedisInstance.lpush("color" + myKey, myVal1, myVal2)
+    val myKey = getJedis().incr("primary")
+    getJedis().lpush("color" + myKey, myVal1, myVal2)
   }
 
   def getAllGradients(): JsValue = {
-    val keys = jedisInstance.keys("color*").asScala.toSet
+    val keys = getJedis().keys("color*").asScala.toSet
     var list = List[Colors]()
     for (key <- keys) {
-      val colors = jedisInstance.lrange(key, 0, -1)
+      val colors = getJedis().lrange(key, 0, -1)
       if (colors.size() != 0) {
         val cObj = Colors(colors.get(0), colors.get(1), key)
         list = cObj +: list
